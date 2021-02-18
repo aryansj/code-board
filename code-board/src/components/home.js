8a5d41e2ff0ffd0a3345e40bb2d3f3d7484c41ad
amritj7 <amritansh7757@gmail.com>
2021-02-18 23:23:01 +0530

    add highlight features

--- code-board/src/components/home.js
+++ code-board/src/components/home.js
@@ -7,9 +7,11 @@ class ImportCode extends React.Component {
     this.state.array = [];
     this.state.currentValue = "";
     this.state.correctChar = 0;
-    this.state.currentIndex = Math.floor(Math.random() * 100);
+    this.state.currentIndex = Math.floor(Math.random() * 4);
     this.state.formVisible = false;
-    this.state.nextIndex = Math.floor(Math.random() * 100);
+    this.state.nextIndex = Math.floor(Math.random() * 4);
+    this.state.highlightChar = -1;
+    this.state.timeleft = 15000;
     this.handleLine = this.handleLine.bind(this);
     this.createform = this.createform.bind(this);
     this.startTime = this.startTime.bind(this);
@@ -18,7 +20,7 @@ class ImportCode extends React.Component {
     this.setState({
       currentIndex: this.state.nextIndex,
       totalChar: this.state.totalChar + Content[this.state.currentIndex].length,
-      nextIndex: Math.floor(Math.random() * 100),
+      nextIndex: Math.floor(Math.random() * 4),
     });
   }
 
@@ -42,14 +44,20 @@ class ImportCode extends React.Component {
             this.setState({
               currentValue: e.target.value,
               correctChar: this.state.correctChar + 1,
+              highlightChar: userString.length - 1,
             });
           }
         }}
         className={classes}
         onKeyPress={(e) => {
-          if (e.key === "Enter") {
+          if (
+            e.key === "Enter" &&
+            this.state.highlightChar ===
+              Content[this.state.currentIndex].length - 1
+          ) {
             this.setState({
               currentValue: "",
+              highlightChar: -1,
             });
             this.handleLine();
           }
@@ -69,41 +77,42 @@ class ImportCode extends React.Component {
       formVisible: true,
       correctChar: 0,
       currentValue: "",
+      highlightChar: -1,
     });
     setTimeout(() => {
       this.setState({
         formVisible: false,
       });
       console.log(this.state.correctChar);
-    }, 5000);
+    }, 15000);
   }
   createform() {
     var classes = "py-3 px-4 w-full ";
     if (!this.state.formVisible) {
       classes = classes + "hidden ";
     }
+    var firstHalf = Content[this.state.currentIndex].slice(
+      0,
+      this.state.highlightChar + 1
+    );
+    var secondHalf = Content[this.state.currentIndex].slice(
+      this.state.highlightChar + 1,
+      Content[this.state.currentIndex].length
+    );
+    console.log(secondHalf);
     return (
       <div>
-        <input
-          type="text"
-          placeholder="current Line!"
-          value={Content[this.state.currentIndex]}
-          className={classes}
-        />
-        <input
-          type="text"
-          placeholder="next line!"
-          value={Content[this.state.nextIndex]}
-          className={classes}
-        />
+        <p className={classes}>
+          <span class="text-green-600">{firstHalf}</span>
+          <span class="">{secondHalf}</span>
+        </p>
+        <p className={classes}>{Content[this.state.nextIndex]}</p>
         {this.renderInput()}
+        <p>{this.state.timeleft}</p>
       </div>
     );
   }
-  handlecode() {
-    console.log(Content.length);
-    return <div>hey</div>;
-  }
+
   render() {
     return (
       <div>
